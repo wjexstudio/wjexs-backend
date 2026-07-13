a9968539f01f5f3cea4d6b7a059088f99e122a5a
wjexstudio <wasin.jex@gmail.com>
2026-07-13 13:12:15 +0700

    chore: clean up unused files

--- test_prisma.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient({ url: process.env.DATABASE_URL });
-prisma.$connect().then(() => console.log("Connected")).catch(e => console.error(e));
